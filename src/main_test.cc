9f89be85ce2325b9592cdd195e6ad9cebc3eb4b1
Philip Salire <philip.s.salire@gmail.com>
2020-03-13 20:27:09 -0700

    test lambda in threadpool

--- src/main_test.cc
+++ src/main_test.cc
@@ -11,7 +11,7 @@ ThreadPool<std::string> t_2(20, fast_test);
 /* Sleep for 1-3 seconds */
 void sleep_test(int i) {
     int time = rand()%3+1;
-    t_1.safe_print("Sleeping for " + std::to_string(time) + " sec...", 0);
+    t_1.safe_print("Sleeping for " + std::to_string(time) + " sec...");
     std::this_thread::sleep_for(std::chrono::seconds(time));
 }
 /* Print a string */
@@ -19,12 +19,13 @@ void fast_test(std::string s) {
     t_2.safe_print("Got: \"" + s + "\"", 0);
 }
 
+/* Test 3 different thread pools */
 int main() {
     /* Start thread pool */
     t_1.start();
     /* Push values to queue, wait 1-2 seconds between each push */
     /* Thread pool automatically starts processing as pushed to queue */
-    for (int i = 0; i < 33; i++) {
+    for (int i = 0; i < 22; i++) {
         t_1.push_to_queue(i);
         t_1.safe_print("Pushed val " + std::to_string(i), 0);
         std::this_thread::sleep_for(std::chrono::seconds(rand()%2+1));
@@ -35,6 +36,8 @@ int main() {
     std::cout << "\n\n--------------------------------\n\n";
 
     /******************************************/
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     /* Start thread pool */
     t_2.start();
     /* Push random strings to queue */
@@ -51,6 +54,24 @@ int main() {
     }
     /* Join all threads in thread pool */
     t_2.join_pool();
+    std::cout << "\n\n--------------------------------\n\n";
+
+    /******************************************/
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    /* Test with lambda and start after queue is filled */
+    ThreadPool<char> t_3(15, [&](char c) {
+        t_3.safe_print(std::to_string((char)c));
+    });
+    /* Push random chars to queue */
+    /* Thread pool automatically starts processing as pushed to queue */
+    for (int i = 0; i < 300; i++) {
+        /* Generate random char */
+       t_3.push_to_queue((char) 33 + (rand()%93));
+    }
+    /* Start after pushing to queue */
+    t_3.start();
+    t_3.join_pool();
 
     return 0;
 }
